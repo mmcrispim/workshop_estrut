39fca4401f17e63f06a132d412353fdddfaac3f6
Mariangela Crispim <mmcrispim@yahoo.com.br>
2024-03-20 16:34:45 -0300

    inclusão isort, blue, pydocstyle, pip-audit e taskipy

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -1,11 +1,11 @@
-import pandas as pd
-import os # biblioteca para manipular arquivos e pastas
-import glob # biblioteca para listar arquivos
+import glob  # biblioteca para listar arquivos
+import os  # biblioteca para manipular arquivos e pastas
 from typing import List
 
-
+import pandas as pd
 
 # path = "data/input"
+
 
 def extract_from_excel(path: str) -> list[pd.DataFrame]:
 
@@ -17,17 +17,18 @@ def extract_from_excel(path: str) -> list[pd.DataFrame]:
     return: lista de dataframes
     """
 
-    all_files = glob.glob(os.path.join(path, "*.xlsx"))
+    all_files = glob.glob(os.path.join(path, '*.xlsx'))
 
     # df_from_each_file = (pd.read_excel(f) for f in all_files)
 
     data_frame_list = []
     for file in all_files:
         data_frame_list.append(pd.read_excel(file))
-    
+
     return data_frame_list
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     # data_frame_list = extract_from_excel(path)
-    data_frame_list = extract_from_excel(path = "data/input")
+    data_frame_list = extract_from_excel(path='data/input')
     print(data_frame_list)
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,8 +1,10 @@
 import pandas as pd
+
 from app.pipeline.transforme import concat_data_frames
 
-df_1 = pd.DataFrame({'col1':[1,2], 'col2':[3,4]})
-df_2 = pd.DataFrame({'col1':[5,6], 'col2':[7,8]})
+df_1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+df_2 = pd.DataFrame({'col1': [5, 6], 'col2': [7, 8]})
+
 
 def testar_concat_list_dataframes():
     # arrange = pd.concat([df_1, df_2], ignore_index=True)
@@ -14,5 +16,5 @@ def testar_concat_list_dataframes():
 
     df = concat_data_frames(data_frame_list)
 
-    assert df.shape == (4,2)
+    assert df.shape == (4, 2)
     assert data_frame.equals(df)
